5355fb972e0adbe8298392d5452fbce3d3e8bad2
Victor Strimbu <vstrimbu@NMBU.NO>
2018-05-15 15:16:33 +0200

    restore

--- R/yasso15.R
+++ R/yasso15.R
@@ -19,10 +19,10 @@
   cat("yasso15 v0.1")
 }
 
-.onLoad = function(){
-  library.dynam('libgcc_s_seh-1.dll', pkg, lib)
-  library.dynam('libquadmath-0.dll', pkg, lib)
-  library.dynam('libwinpthread-1.dll', pkg, lib)
-  library.dynam('libgfortran-4.dll', pkg, lib)
-  library.dynam('yasso15.dll', pkg, lib)
-}
+# .onLoad = function(){
+#   library.dynam('libgcc_s_seh-1.dll', pkg, lib)
+#   library.dynam('libquadmath-0.dll', pkg, lib)
+#   library.dynam('libwinpthread-1.dll', pkg, lib)
+#   library.dynam('libgfortran-4.dll', pkg, lib)
+#   library.dynam('yasso15.dll', pkg, lib)
+# }
