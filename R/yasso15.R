e491dd1b0e59b2c5ed8e6f83ab0cadfe1f681d50
Victor Strimbu <vstrimbu@NMBU.NO>
2018-05-18 16:02:46 +0200

    dll debug

--- R/yasso15.R
+++ R/yasso15.R
@@ -6,6 +6,9 @@
   filepath = paste0(getwd(),"\\external\\libgcc_s_seh-1.dll")
   cat(filepath,": ",file.exists(filepath))
 
+  filepath = paste0(getwd(),"\\data\\turnover.csv")
+  cat(filepath,": ",file.exists(filepath))
+
   dyn.load("external\\libgcc_s_seh-1.dll")
   dyn.load("external\\libquadmath-0.dll")
   dyn.load("external\\libwinpthread-1.dll")
