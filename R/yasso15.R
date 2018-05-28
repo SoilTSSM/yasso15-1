0e5ec488c628df6e5cb41116578e567205cf64b5
Victor Strimbu <vstrimbu@NMBU.NO>
2018-05-28 14:32:27 +0200

    move dll import to .onLoad

--- R/yasso15.R
+++ R/yasso15.R
@@ -3,9 +3,7 @@
   library(matrixStats)
   library(skogR)
   # load external binaries;
-  d1.file = system.file("dlls", "libgcc_s_seh-1.dll", package = "yasso15")
-  cat(d1.file)
-  dyn.load(d1.file)
+
   #dyn.load(paste0(path.package("yasso15"),"\\external\\libquadmath-0.dll"))
   #dyn.load(paste0(path.package("yasso15"),"\\external\\libwinpthread-1.dll"))
   #dyn.load(paste0(path.package("yasso15"),"\\external\\libgfortran-4.dll"))
@@ -16,10 +14,8 @@
   cat("yasso15 v0.1")
 }
 
-# .onLoad = function(){
-#   library.dynam('libgcc_s_seh-1.dll', pkg, lib)
-#   library.dynam('libquadmath-0.dll', pkg, lib)
-#   library.dynam('libwinpthread-1.dll', pkg, lib)
-#   library.dynam('libgfortran-4.dll', pkg, lib)
-#   library.dynam('yasso15.dll', pkg, lib)
-# }
+ .onLoad = function(){
+   d1.file = system.file("dlls", "libgcc_s_seh-1.dll", package = "yasso15")
+   cat(d1.file)
+   dyn.load(d1.file)
+ }
